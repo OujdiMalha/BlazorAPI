ea0bdadbc0e04cf675512b72a8806ad5192a3ab0
OujdiMalha <mohammed.malha@ifosup.wavre.be>
2024-03-20 11:53:20 +0100

    Page d'acceuil HTML ok

--- Util/PokeClient.cs
+++ Util/PokeClient.cs
@@ -1,21 +1,31 @@
-﻿using System.Text.Json;
+﻿using BlazorAPI.Models;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace BlazorAPI.Util
 {
     public class PokeClient
     {
-        public HttpClient HttpClient { get; set; }
-        
-        public PokeClient(HttpClient client)
+        private readonly HttpClient _httpClient;
+
+        public PokeClient(HttpClient httpClient)
         {
-            this.Client = client;
+            _httpClient = httpClient;
         }
+
         public async Task<Pokemon> GetPokemon(string id)
         {
-            var response = await this.Client.GetAsync($"https://pokeapi.co/api/v2/pokemon/{id}");
+            var response = await _httpClient.GetAsync($"https://pokeapi.co/api/v2/pokemon/{id}");
+            response.EnsureSuccessStatusCode(); // Optional: Ensure response is successful before reading content
+
             var content = await response.Content.ReadAsStringAsync();
-            
-            return JsonSerializer.Deserialize<Pokemon>(content);
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            return JsonSerializer.Deserialize<Pokemon>(content, options);
         }
     }
 }
