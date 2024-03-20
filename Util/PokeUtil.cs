c400c1ae97bc93daf5531f640bcac14fe2d6323b
OujdiMalha <mohammed.malha@ifosup.wavre.be>
2024-03-20 23:41:45 +0100

    fin de la classe pokeUtil (manque des modifications)

---
+++ Util/PokeUtil.cs
@@ -0,0 +1,33 @@
+﻿namespace BlazorAPI.Util
+{
+    public static class PokeUtil
+    {
+        public const string pokeball = "icons8-pokeball-48";
+
+        public const string Greatball = "icons8-pokeball-48";
+
+        public const string Ultraball = "icons8-pokeball-48";
+
+        public const string MasterBall = "icons8-pokeball-48";
+
+        public static string GetRandomBall()
+        {
+            var pokeballArray = new String[] {pokeball, Greatball, Ultraball, MasterBall};
+
+            var rand = new Random();
+            string randamball = pokeballArray[rand.Next(pokeballArray.Length)];
+
+            return randamball;
+        }
+
+        public static double GetPokemonHeightInMeters(int height)
+        {
+            return Convert.ToDouble(height) / 10;
+        }
+
+        public static double GetPokemonWeightInKilograms(int weight)
+        {
+            return Convert.ToDouble(weight) / 10;
+        }
+    }
+}
