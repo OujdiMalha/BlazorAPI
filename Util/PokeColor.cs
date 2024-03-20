20011db0fdd639fece9873398fc5bd94d5b69af1
OujdiMalha <mohammed.malha@ifosup.wavre.be>
2024-03-20 14:35:07 +0100

    CSS version 2

--- Util/PokeColor.cs
+++ Util/PokeColor.cs
@@ -1,6 +1,31 @@
 ﻿namespace BlazorAPI.Util
 {
-    public class PokeColor
+    public static class PokeColor
     {
+        public static string GetTypeColor(string type)
+        {
+            string color = type switch
+            {
+                "bug" => "#26de81",
+                "dragon" => "#ffeaa7",
+                "electric" => "#fed330",
+                "fairy" => "#FF0069",
+                "fighting" => "#30336b",
+                "fire" => "#f0932b",
+                "flying" => "#81ecec",
+                "grass" => "#00b894",
+                "ground" => "#EFB549",
+                "ghost" => "#a55eea",
+                "ice" => "#74b9ff",
+                "normal" => "#95afc0",
+                "poison" => "#9400D3",
+                "psychic" => "#a29bfe",
+                "rock" => "#2d3436",
+                "water" => "#0190FF",
+                _ => "#0190FF" // Default value if no match is found
+            };
+
+            return color; // Return the color value
+        }
     }
 }
